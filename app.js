6681a6c92f6666d1857cbcbecdc7c5d79492134d
GabrielDko <gabriielrobless@gmail.com>
2023-10-12 21:50:08 -0300

    Archivos con la estructura básica y levante del funcionamiento de la página Emilia Cole

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express();
+const path = require("path")
+
+app.listen(3030, ()=> {console.log("Arrancando el servidor en la ruta 3030")})
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+app.get("/views/home.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+app.get("/views/about.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/about.html"))
+})
+app.get("/views/music.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/music.html"))
+})
+
+app.get("/views/contact.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/contact.html"))
+})
+
+app.use(express.static("public"))
